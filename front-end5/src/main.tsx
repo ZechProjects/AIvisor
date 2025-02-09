d4be9050756ee87d3b701d403eedda096f8c11cd
Zech <166375739+ZechGvrn@users.noreply.github.com>
2025-02-09 21:18:42 +0800

    fix privy error

--- front-end5/src/main.tsx
+++ front-end5/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from "./App";
 import "./index.css";
+import { AppProvider } from "./contexts/AppContext";
 
 import { PrivyProvider } from "@privy-io/react-auth";
 
@@ -30,9 +31,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           },
         }}
       >
-        {/* <QueryClientProvider client={queryClient}> */}
-        <App />
-        {/* </QueryClientProvider> */}
+        <AppProvider>
+          {/* <QueryClientProvider client={queryClient}> */}
+          <App />
+          {/* </QueryClientProvider> */}
+        </AppProvider>
       </PrivyProvider>
     </BrowserRouter>
   </React.StrictMode>
