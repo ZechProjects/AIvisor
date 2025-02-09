3a7aca5c2ca07f9869c3cf09773bc820a5d48f46
Zech <166375739+ZechGvrn@users.noreply.github.com>
2025-02-09 21:15:59 +0800

    add privy login/signup

--- front-end5/src/main.tsx
+++ front-end5/src/main.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
-import './index.css'
-import { AppProvider } from './contexts/AppContext'
+import App from "./App";
+import "./index.css";
+
+import { PrivyProvider } from "@privy-io/react-auth";
 
 // Create a client for React Query
 // const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppProvider>
-      {/* <QueryClientProvider client={queryClient}> */}
+      <PrivyProvider
+        appId="cm6xiefo8007xenxlzqc6pqjh"
+        config={{
+          // Display email and wallet as login methods
+          loginMethods: ["email", "wallet"],
+          // Customize Privy's appearance in your app
+          appearance: {
+            theme: "light",
+            accentColor: "#676FFF",
+            logo: "https://your-logo-url",
+          },
+          // Create embedded wallets for users who don't have a wallet
+          embeddedWallets: {
+            createOnLogin: "users-without-wallets",
+          },
+        }}
+      >
+        {/* <QueryClientProvider client={queryClient}> */}
         <App />
-      {/* </QueryClientProvider> */}
-      </AppProvider>
+        {/* </QueryClientProvider> */}
+      </PrivyProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
